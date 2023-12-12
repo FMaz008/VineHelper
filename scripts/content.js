b79dc84dd47f38836a403944d6508b3d9b75b46e
FMaz <fmaz008@gmail.com>
2023-12-12 10:04:07 -0400

    FIX: - Resources not loading properly because of the hardcoded extension ID
    CHANGES:
    - Addition of an Info icon
    - Now requiring to click "Check import fees" instead of auto-checking on hovering the items.

--- scripts/content.js
+++ scripts/content.js
@@ -1,57 +1,85 @@
 
-$('head').append( $('<link rel="stylesheet" type="text/css" />').attr('href', 'moz-extension://5951bf9a-25ae-4043-9fa2-54dc0ac1ce70/main.css') );
+$('head').append( $('<link rel="stylesheet" type="text/css" />').attr('href', browser.runtime.getURL('/main.css') ));
+//$('head').append( $('<script type="text/javascript" />').attr('src', browser.runtime.getURL('/scripts/content.js') ));
 
-$(".vvp-item-tile-content").hover(function() {
+let i=0;
+$(".vvp-item-tile-content").each(function(){
+	$(this).prepend(
+		'<div class="ext-helper-status">'
+			+ '<div class="ext-helper-status-container">'
+				+ '<div class="ext-helper-icon ext-helper-icon-info"></div>'
+				+ '<span>'
+					+ '<a href="#" id="ext-helper-link-importfees-'+i+'" onclick="return false;">'
+						+ 'Check import fees'
+					+ '</a>'
+				+ '</span>'
+			+ '</div>'
+		+ '</div>'
+	);
 	
-	if ($(this).hasClass("ext-helper") == true) 
-		return; //We already check for this item, we don't want to flood vine with XHR requests
-	else
-		$( this ).addClass( "ext-helper" ); //Add a class to the product card to avoid re-processing it
+	
+	$("a#ext-helper-link-importfees-" + i).click(function(){
+		let itemContext = $(this).parents(".vvp-item-tile-content");
 		
-	$(this).prepend('<div class="ext-helper-status"><div class="ext-helper-status-container"><div class="ext-helper-status-icon"></div><span>Loading...</span></div></div>');
-	
-	
-	//Find the item page URL:
-	let url = $(this).find(".a-link-normal").attr("href");
-	
-	//Load the page via an Ajax request
-	$.ajax(url,   // request url
-		{
-			context: this,
-			success: function (data, status, xhr) {// success callback function
-				parseDOM (this, data);
-		}
+		if ($(itemContext).hasClass("ext-helper") == true) 
+			return; //We already check for this item, we don't want to flood vine with XHR requests
+		else
+			$(itemContext).addClass( "ext-helper" ); //Add a class to the product card to avoid re-processing it
+			
+		$(itemContext).find("div.ext-helper-status div span").text("Loading...");
+		
+		
+		//Find the item page URL:
+		let url = $(itemContext).find(".a-link-normal").attr("href");
+		
+		//Load the page via an Ajax request
+		$.ajax(url,   // request url
+			{
+				context: itemContext,
+				success: function (data, status, xhr) {// success callback function
+					parseDOM (this, data);
+				}
+			}
+		);
 	});
 	
-}, function() {});
+	i++;
+});
+
 
 function parseDOM(context, data){
-		
+	
+	//remove the info icon
+	$( context ).find(".ext-helper-icon").removeClass("ext-helper-icon-info");
+	
+	//Look for standard import fees
 	let normalImportFees = $(data).find("span#priceblock_ourprice_ifdmsg");
 	if(normalImportFees.length>0){
 
-		$( context ).find(".ext-helper-status-icon").addClass("ext-helper-icon-yes");
+		$( context ).find(".ext-helper-icon").addClass("ext-helper-icon-yes");
 		$( context ).find(".ext-helper-status-container span").text(normalImportFees[0].innerText);
 		$(context).find("img").css('opacity', '0.3');
 		return;
 	}
 	
+	//Look for import fees included in the price
 	let includedImportFees  = $(data).find("span#priceblock_dealprice_ifdmsg");
 	if(includedImportFees.length>0){
-		$( context ).find(".ext-helper-status-icon").addClass("ext-helper-icon-yes");
+		$( context ).find(".ext-helper-icon").addClass("ext-helper-icon-yes");
 		$( context ).find(".ext-helper-status-container span").text("Deal price (Included import fees)");
 		$(context).find("img").css('opacity', '0.3');
 		return;
 	}
 	
+	//Look if the item has variations
 	let variationsFormDetected  = $(data).find("div#twister_feature_div form#twister");
 	if(variationsFormDetected.length>0){
 		$( context ).find(".ext-helper-status-container span").text("Variations detected. Unable to guess.");
 		return;
 	}
 	
-	
-	$( context ).find(".ext-helper-status-icon").addClass("ext-helper-icon-no");
+	//If nothing, it's likely to have no import fees
+	$( context ).find(".ext-helper-icon").addClass("ext-helper-icon-no");
 	$( context ).find(".ext-helper-status-container span").text("No import fees detected");
 		
 }
