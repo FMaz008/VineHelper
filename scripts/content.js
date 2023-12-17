70e1d3f3eeadd8f9e35a625d4777d342298eff78
FMaz <fmaz008@gmail.com>
2023-12-17 12:53:56 -0400

    Display system
    FIX:
    - Using Fetch instead of JSONP to get page data from the server
    
    ADD:
    - Display Loading... while contacting the server
    - Display the proper icon&status based on the server's reply
    - Interface to submit a vote on an item, with refresh of the item display
    
    TODO:
    Submit an actual query to the server to register a vote on an item.

--- scripts/content.js
+++ scripts/content.js
@@ -1,44 +1,86 @@
 
 
+
 //Create an array of all the products listed on the page
+var pageId; //Will be used to store the current pageId within the each loop.
 var arrUrl = [];
 const regex = /^(?:.*\/dp\/)(.+?)(?:\?.*)?$/; //Isolate the product ID in the URL.
 
 $(".vvp-item-tile-content").each(function(){
 	
 	let url = $(this).find(".a-link-normal").attr("href");
-	let pageId = url.match(regex);
-	arrUrl.push(pageId[1]);
+	let arrPageId = url.match(regex);
+	pageId = arrPageId[1];
+	arrUrl.push(pageId);
 
+	$(this).prepend(
+		'<div id="ext-helper-toolbar-' + pageId + '" class="ext-helper-status">'
+			+ '<div class="ext-helper-status-container">'
+				+ '<div class="ext-helper-icon ext-helper-icon-info"></div>'
+				+ '<span>Loading...</span>'
+			+ '</div>'
+		+ '</div>'
+	);
+	
+	
 });
-let jsonArrURL = JSON.stringify(arrUrl);
-console.log(jsonArrURL);
+
+
 
 //Post an AJAX request to the 3rd party server, passing along the JSON array of all the products on the page
-url = "https://www.francoismazerolle.ca/vinehelper.php"
+let jsonArrURL = JSON.stringify(arrUrl);
+let url = "https://www.francoismazerolle.ca/vinehelper.php"
 		+ "?data=" + jsonArrURL;
-$.ajax(url,   // request url
-			{
-				crossDomain: true,
-				headers: { /* Trying something, no change */
-					'Access-Control-Allow-Origin': '*',
-					"Referrer-Policy": "unsafe-url",
-				},
-				xhrFields: { /* Trying something, no change */
-					withCredentials: true
-				},
-				beforeSend: function( xhr ) { /* Trying something, no change */
-					xhr.setRequestHeader( "Referrer-Policy" , "unsafe-url" );
-				},
-				dataType: 'jsonp',
-				jsonp: 'callback',
-				jsonpCallback: 'jsonpServerResponse',
-			}
-		);
+fetch(url)
+    .then((response) => response.json())
+    .then(serverResponse);
 
-function jsonpServerResponse(data){
+
+function serverResponse(data){
 	//Print out the result from the ajax query.
 	console.log(data);
+	//let arr = $.parseJSON(data); //convert to javascript array
+	$.each(data,function(key,value){
+		updateToolBarFees(key, value);
+	});
 }
 
+function updateToolBarFees(pageId, fees){
+	let context = $("#ext-helper-toolbar-" + pageId);
+	
+	//Remove the default icon
+	$(context).find(".ext-helper-icon").removeClass("ext-helper-icon-info");
+	
+	if(fees==1){
+		//The item has fees
+		$(context).find(".ext-helper-icon").addClass("ext-helper-icon-yes");
+		$(context).find("div.ext-helper-status-container span").text("Import fees reported");
+		$(context).parent(".vvp-item-tile-content").find("img").css('opacity', '0.3');
+	}else if(fees==0){
+		//The item does not have fees
+		$(context).find(".ext-helper-icon").addClass("ext-helper-icon-no");
+		$(context).find("div.ext-helper-status-container span").text("No import fees!");
+	}else if(fees==null){
+		//The item is not registered or needs more votes
+		$(context).find(".ext-helper-icon").addClass("ext-helper-icon-info");
+		$(context).find("div.ext-helper-status-container span").html(
+			"No data :-/<br />"
+			+ "Any fees? "
+			+ "<a href='#"+pageId+"' id='ext-helper-reportlink-"+pageId+"-yes' onclick='return false;'>Yes</a> / "
+			+ "<a href='#"+pageId+"' id='ext-helper-reportlink-"+pageId+"-no' onclick='return false;'>No</a>"
+		);
+		
+		$("a#ext-helper-reportlink-"+pageId+"-yes").bind('click', { 'pageId': pageId, 'fees': 1 }, reportfees);
+		$("a#ext-helper-reportlink-"+pageId+"-no").bind('click', { 'pageId': pageId, 'fees': 0 }, reportfees);
+	}
+}
+
+
+
+var reportfees = function(event){
+	let pageId = event.data.pageId;
+	let fees = event.data.fees;
+	console.log("Reporting page " +pageId+ " with fee value of " + fees);
+	updateToolBarFees(pageId, fees);
+};
 
