979f030685f254db1a81ebc42e07a27bda9dbfca
FMaz <fmaz008@gmail.com>
2023-12-21 14:21:48 -0400

    Refactoring to improve the code flow and function order. Improved logic of what code goes in which function. Added animation when sending a product in or out of the Discard grid.

--- scripts/content.js
+++ scripts/content.js
@@ -50,7 +50,79 @@ async function getSettings(){
 getSettings();
 
 
+//#########################
+//### Utility functions
 
+function toggleDiscardedList(){
+	$('#ext-helper-grid').toggle();
+	if($('#ext-helper-grid').is(":hidden")){
+		$("#ext-helper-grid-collapse-indicator").html("&#11166;");	
+	}else{
+		$("#ext-helper-grid-collapse-indicator").html("&#11167;");
+	}
+}
+
+function getTileGrid(pageId, gridId){
+	let grid = $("#ext-helper-toolbar-" + pageId).parents(gridId); //Time consuming, used in a loop
+	if(grid.length==0)
+		return null;
+	return grid;
+}
+
+async function moveProductTileToGrid(pageId, gridSelector, animate=false){
+	
+	if(animate)
+		await animateVanish(pageId);
+	
+	let tile = $("#ext-helper-toolbar-" + pageId).parents(".vvp-item-tile");
+	$(tile).detach().appendTo(gridSelector);
+}
+
+async function animateVanish(pageId){
+	let tile = $("#ext-helper-toolbar-" + pageId).parent(".vvp-item-tile-content");
+	let defaultOpacity = $(tile).css("opacity");
+	await tile.animate({
+		height: "hide",
+		opacity: "hide"
+	},
+	{
+		duration: 500,
+		complete: function() {
+			$(tile).show();
+			$(tile).css('opacity', defaultOpacity);
+		}
+	}).promise(); //Converting the animation to a promise allow for the await clause to work.
+}
+
+
+
+
+//#########################
+//### Main flow
+
+
+//Initiate the extension
+function init(){
+	
+	//Create the Discard grid
+	createInterface();
+
+	//Browse each items from the Regular grid
+	//- Create an array of all the products listed on the page
+	//- Create an empty toolbar for the item tile
+	$(".vvp-item-tile-content").each(function(){
+		
+		let url = $(this).find(".a-link-normal").attr("href");
+		let arrPageId = url.match(regex);
+		pageId = arrPageId[1];
+		arrUrl.push(pageId);
+
+		createProductToolbar(this, pageId);
+	});
+
+	//Obtain the data to fill the toolbars with it.
+	fetchData(arrUrl);
+}
 
 function createInterface(){
 	//Clean up interface (in case of the extension being reloaded)
@@ -82,14 +154,6 @@ function createInterface(){
 	toggleDiscardedList(); //Hide at first
 }
 
-function toggleDiscardedList(){
-	$('#ext-helper-grid').toggle();
-	if($('#ext-helper-grid').is(":hidden")){
-		$("#ext-helper-grid-collapse-indicator").html("&#11166;");	
-	}else{
-		$("#ext-helper-grid-collapse-indicator").html("&#11167;");
-	}
-}
 
 function createProductToolbar(attachTo, pageId){
 	let toolbarId = "ext-helper-toolbar-" + pageId;
@@ -108,6 +172,91 @@ function createProductToolbar(attachTo, pageId){
 		.text("Loading...")
 		.appendTo("#"+toolbarId + " .ext-helper-status-container");
 }
+
+
+
+//Get data from the server about the products listed on this page
+function fetchData(arrUrl){
+	let arrJSON = {"api_version":2, "arr_url":arrUrl};
+	let jsonArrURL = JSON.stringify(arrJSON);
+	
+	//Post an AJAX request to the 3rd party server, passing along the JSON array of all the products on the page
+	let url = "https://www.francoismazerolle.ca/vinehelper_v2.php"
+			+ "?data=" + jsonArrURL;
+	fetch(url)
+		.then((response) => response.json())
+		.then(serverResponse)
+		.catch( error =>  console.log(error) );
+}
+
+function serverResponse(data){
+	//let arr = $.parseJSON(data); //convert to javascript array
+	if(data["api_version"]!=2){
+		console.log("Wrong API version");
+	}
+	
+	$.each(data["arr_url"],function(key,values){
+		
+		//If there is a consensus of a fee for the product, move it to the discarded grid
+		if(values["v1"] - values["v0"] >= consensusThreshold){
+			moveProductTileToGrid(key, '#ext-helper-grid', false); //This is the main sort, do not animate it
+		}else{
+			//We voted "fees" + self discard option is on: move the item on the discard grid
+			if(values["s"] == 1 && selfDiscard == true){
+				moveProductTileToGrid(key, '#ext-helper-grid', false); //This is the main sort, do not animate it
+			}
+		}
+				
+		//Update the toolbar with the information received
+		updateToolBarFees(key, values);
+	});
+	
+	//Calculate how many tiles were moved to the discarded grid
+	$("#ext-helper-grid-count").text($("#ext-helper-grid").children().length);
+}
+
+function updateToolBarFees(pageId, arrValues){
+	let context = $("#ext-helper-toolbar-" + pageId);
+	let icon = $(context).find(".ext-helper-icon");
+	let container = $(context).find("div.ext-helper-status-container2");
+	
+	let fees = arrValues["f"];
+	let voteUser = arrValues["s"];
+	let votesFees = arrValues["v1"];
+	let votesNoFees = arrValues["v0"];
+	
+	//Remove all images for the icon
+	icon.removeClass("ext-helper-icon-info");
+	icon.removeClass("ext-helper-icon-sad");
+	icon.removeClass("ext-helper-icon-happy");
+	
+	let tileOpacity;
+	let statusText;
+	let statusIcon;
+	if(fees==1 || (fees==null && voteUser==1)){
+		//The item has fees
+		statusIcon = "ext-helper-icon-sad";
+		statusText = "Import fees reported";
+		tileOpacity = 0.3;
+	}else if(fees==0 || (fees==null && voteUser==0)){
+		//The item does not have fees
+		statusIcon = "ext-helper-icon-happy";
+		statusText = "No import fees!";
+		tileOpacity = 1.0;
+	}else if(fees==null){
+		//The item is not registered or needs more votes
+		statusIcon = "ext-helper-icon-info";
+		statusText = "Not enough data :-/";
+		tileOpacity = 1.0;
+	}
+	
+	$(context).parent(".vvp-item-tile-content").css('opacity', tileOpacity);
+	icon.addClass(statusIcon);
+	container.text(statusText);
+	
+	createVotingWidget(pageId, votesFees, votesNoFees, voteUser);
+}
+
 
 function createVotingWidget(pageId, votesFees, votesNoFees, voteUser){
 	let context = $("#ext-helper-toolbar-" + pageId);
@@ -143,136 +292,40 @@ function createVotingWidget(pageId, votesFees, votesNoFees, voteUser){
 	//      If we have not voted, we want the option to remain visible.
 	pe.css('opacity', (voteUser == 0) ? 0.4 : 1.0); 
 	
-	//If the user has already voted, add the selected class to the vote link
 	if(voteUser == 1){
 		v1.addClass("selectedVote");
-		
-		//If self discard is on, move the item on the discard grid
-		if(selfDiscard == true){
-			moveProductTileToGrid(pageId, '#ext-helper-grid');
-		}
 	}
 	if(voteUser == 0){
 		v0.addClass("selectedVote");
-		
-		//We vote nofees and there's no more consensus, move the item out of the discard grid
-		if(votesFees - votesNoFees < consensusThreshold){
-			let grid = $("#ext-helper-toolbar-" + pageId).parents("#ext-helper-grid");
-			if(grid.length==1){ //Item is located in the discard grill
-				moveProductTileToGrid(pageId, '#vvp-items-grid');
-			}
-		}
 	}
 }
 
-function moveProductTileToGrid(pageId, gridSelector){
-	let tile = $("#ext-helper-toolbar-" + pageId).parents(".vvp-item-tile");
-	$(tile).detach().appendTo(gridSelector);
-}
 
-//Initiate the extension
-function init(){
-	createInterface();
-
-	//Browse each items from the list
-	//Create an array of all the products listed on the page
-	$(".vvp-item-tile-content").each(function(){
-		
-		let url = $(this).find(".a-link-normal").attr("href");
-		let arrPageId = url.match(regex);
-		pageId = arrPageId[1];
-		arrUrl.push(pageId);
-
-		createProductToolbar(this, pageId);
-	});
-
-	fetchData(arrUrl);
-}
-
-
-//Get data from the server about the products listed on this page
-function fetchData(arrUrl){
-	let arrJSON = {"api_version":2, "arr_url":arrUrl};
-	let jsonArrURL = JSON.stringify(arrJSON);
-	
-	//Post an AJAX request to the 3rd party server, passing along the JSON array of all the products on the page
-	let url = "https://www.francoismazerolle.ca/vinehelper_v2.php"
-			+ "?data=" + jsonArrURL;
-	fetch(url)
-		.then((response) => response.json())
-		.then(serverResponse)
-		.catch( error =>  console.log(error) );
-}
-
-function serverResponse(data){
-	//let arr = $.parseJSON(data); //convert to javascript array
-	if(data["api_version"]!=2){
-		console.log("Wrong API version");
-	}
-	
-	$.each(data["arr_url"],function(key,values){
-		
-		//If there is a consensus of a fee for the product, move it to the discarded grid
-		if(values["v1"] - values["v0"] >= consensusThreshold){
-			moveProductTileToGrid(key, '#ext-helper-grid');
-		}
-		
-		//Update the toolbar with the information received
-		updateToolBarFees(key, values);
-	});
-	
-	//Calculate how many tiles were moved to the discarded grid
-	$("#ext-helper-grid-count").text($("#ext-helper-grid").children().length);
-}
-
-function updateToolBarFees(pageId, arrValues){
-	let context = $("#ext-helper-toolbar-" + pageId);
-	let icon = $(context).find(".ext-helper-icon");
-	let container = $(context).find("div.ext-helper-status-container2");
-	
-	let fees = arrValues["f"];
-	let voteUser = arrValues["s"];
-	let votesFees = arrValues["v1"];
-	let votesNoFees = arrValues["v0"];
-	
-	
-	//Remove all images for the icon
-	icon.removeClass("ext-helper-icon-info");
-	icon.removeClass("ext-helper-icon-sad");
-	icon.removeClass("ext-helper-icon-happy");
-	
-	let tileOpacity;
-	let statusText;
-	let statusIcon;
-	if(fees==1 || (fees==null && voteUser==1)){
-		//The item has fees
-		statusIcon = "ext-helper-icon-sad";
-		statusText = "Import fees reported";
-		tileOpacity = 0.3;
-	}else if(fees==0 || (fees==null && voteUser==0)){
-		//The item does not have fees
-		statusIcon = "ext-helper-icon-happy";
-		statusText = "No import fees!";
-		tileOpacity = 1.0;
-	}else if(fees==null){
-		//The item is not registered or needs more votes
-		statusIcon = "ext-helper-icon-info";
-		statusText = "Not enough data :-/";
-		tileOpacity = 1.0;
-	}
-	
-	$(context).parent(".vvp-item-tile-content").css('opacity', tileOpacity);
-	icon.addClass(statusIcon);
-	container.text(statusText);
-	
-	createVotingWidget(pageId, votesFees, votesNoFees, voteUser);
-}
-
-
-
+//A vote button was pressed
 async function reportfees(event){
 	let pageId = event.data.pageId;
 	let fees = event.data.fees;
+	let votesFees = event.data.v1;
+	let votesNoFees = event.data.v0;
+	
+	
+	let grid = null;
+	
+	//Our vote "Fees" + the self discard option is active: move the item to the Discard grid
+	if(fees == 1 && selfDiscard){
+		grid = getTileGrid(pageId, "#vvp-items-grid");
+		if(grid !== null){ //Item is located in the regular grid, but should not
+			await moveProductTileToGrid(pageId, "#ext-helper-grid", true);
+		}
+	}
+	
+	//Our vote is "nofees" + there's no consensus, yet the item is still in the Discard Grid: move the item to the regular grid
+	if(fees == 0 && votesFees - votesNoFees < consensusThreshold){
+		grid = getTileGrid(pageId, "#ext-helper-grid");
+		if(grid !== null){ //Item is located in the discard grid, but should not
+			moveProductTileToGrid(pageId, '#vvp-items-grid', true);
+		}
+	}
 	
 	let url = "https://francoismazerolle.ca/vinehelperCastVote_v2.php"
 		+ '?data={"url":"' + pageId +'","fees":'+ fees +'}';
@@ -281,4 +334,6 @@ async function reportfees(event){
 	let arrUrl = [pageId];
 	fetchData(arrUrl); //Refresh the toolbar for that specific product only
 };
+
+
 
