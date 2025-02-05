a54e84a53320b0f0893a9a7d978434e60ba7c3ad
FMaz <fmaz008@gmail.com>
2025-02-05 09:55:22 -0400

    Removed useless import

--- page/notification_monitor_light.js
+++ page/notification_monitor_light.js
@@ -1,6 +1,3 @@
-import { SettingsMgr } from "../scripts/SettingsMgr.js";
-const Settings = new SettingsMgr();
-
 import { NotificationMonitor } from "../scripts/NotificationMonitor.js";
 const NotifMon = new NotificationMonitor();
 NotifMon.initializeLight();
