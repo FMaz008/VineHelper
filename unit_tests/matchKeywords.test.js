1c886d3634c1db4c3eeece080e208f83dd7821d7
Francois Mazerolle <fmaz008@gmail.com>
2025-05-19 12:27:08 -0300

    Unit test case for 0etv product

--- unit_tests/matchKeywords.test.js
+++ unit_tests/matchKeywords.test.js
@@ -238,3 +238,10 @@ test("ETV_min_out_of_range_values", () => {
 	const str = "TV";
 	expect(keywordMatch(arrKWs, str, 300, 700)).toBe(false);
 });
+
+test("ETV_min_max_zero_values", () => {
+	const arrKWs = [{ contains: "nuts", without: "", etv_min: "0", etv_max: "0" }];
+	const str =
+		"1Pc Silver Mini Adjustable Wrench Adjustable Spanner,Mini Repair Maintenance Hand Tool for Tightening or Loosening,Nuts and Bolts,Wrenches,Power and Hand Tools,Small Shifting Spanner";
+	expect(keywordMatch(arrKWs, str, 5.99, 5.99)).toBe(false);
+});
