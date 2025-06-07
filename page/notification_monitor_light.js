d3624bf0ad18fcb95d5a27bb9edc016308dc4392
Francois Mazerolle <fmaz008@gmail.com>
2025-06-07 14:18:09 -0300

    Load proper CSS stylesheet based on the browser.

--- page/notification_monitor_light.js
+++ page/notification_monitor_light.js
@@ -1,3 +1,15 @@
 import { NotificationMonitorV2 } from "../scripts/notification_monitor/NotificationMonitorV2.js";
 const NotifMon = new NotificationMonitorV2();
 NotifMon.initialize();
+
+//If browser is firefox, load icon_firefox.css
+if (navigator.userAgent.includes("Firefox")) {
+	document.head.innerHTML += `<link rel="stylesheet" type="text/css" href="../resource/css/icon_firefox.css" />`;
+}
+//If the browser is chrome, load icon_chrome.css
+if (navigator.userAgent.includes("Chrome") || navigator.userAgent.includes("Chromium")) {
+	document.head.innerHTML += `<link rel="stylesheet" type="text/css" href="../resource/css/icon_chrome.css" />`;
+}
+if (navigator.userAgent.includes("Safari")) {
+	document.head.innerHTML += `<link rel="stylesheet" type="text/css" href="../resource/css/icon_safari.css" />`;
+}
