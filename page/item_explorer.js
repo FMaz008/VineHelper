21f9179eb4cb963d45c3f4a66c39d8c84a353947
Francois Mazerolle <fmaz008@gmail.com>
2025-06-16 08:14:50 -0300

    Navigator based Stylesheet loading

--- page/item_explorer.js
+++ page/item_explorer.js
@@ -20,6 +20,18 @@ import { unescapeHTML, removeSpecialHTML } from "../scripts/StringHelper.js";
 let secondsLeft = 10;
 
 (async () => {
+	//If browser is firefox, load icon_firefox.css
+	if (navigator.userAgent.includes("Firefox")) {
+		document.head.innerHTML += `<link rel="stylesheet" type="text/css" href="../resource/css/icon_firefox.css" />`;
+	}
+	//If the browser is chrome, load icon_chrome.css
+	if (navigator.userAgent.includes("Chrome") || navigator.userAgent.includes("Chromium")) {
+		document.head.innerHTML += `<link rel="stylesheet" type="text/css" href="../resource/css/icon_chrome.css" />`;
+	}
+	if (navigator.userAgent.includes("Safari")) {
+		document.head.innerHTML += `<link rel="stylesheet" type="text/css" href="../resource/css/icon_ios.css" />`;
+	}
+
 	await Settings.waitForLoad();
 
 	//Check if the membership is valid
