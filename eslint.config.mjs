105f5a39f61072593cb9100373e914e8ecf60aef
Francois Mazerolle <fmaz008@gmail.com>
2025-06-20 17:11:58 -0300

    Added converted eslint config file.

---
+++ eslint.config.mjs
@@ -0,0 +1,35 @@
+import { defineConfig } from "eslint/config";
+import globals from "globals";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import js from "@eslint/js";
+import { FlatCompat } from "@eslint/eslintrc";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+    baseDirectory: __dirname,
+    recommendedConfig: js.configs.recommended,
+    allConfig: js.configs.all
+});
+
+export default defineConfig([{
+    extends: compat.extends("eslint:recommended", "prettier"),
+
+    languageOptions: {
+        globals: {
+            ...globals.browser,
+            ...globals.node,
+        },
+
+        ecmaVersion: 2022,
+        sourceType: "module",
+    },
+
+    rules: {
+        "no-console": "warn",
+        "no-undef": "warn",
+        "no-unsafe-finally": "warn",
+        "no-unused-vars": "warn",
+    },
+}]);
