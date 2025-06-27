87b38b96604d77343c67a3c0c6e5fea7e9599e01
Francois Mazerolle <fmaz008@gmail.com>
2025-06-27 16:11:21 -0300

    Updated evaluations metrics to v1.4.1 ( https://github.com/robartsd/VineTools/blob/main/evaluationMetrics.user.js )

--- scripts/bootloader.js
+++ scripts/bootloader.js
@@ -599,72 +599,93 @@ function displayAccountDataEvaluationMetrics() {
 		return false; //Setting not activated.
 	}
 
-	const evalStartStamp = document.querySelector("#vvp-eval-start-stamp");
-	const evalEndStamp = document.querySelector("#vvp-eval-end-stamp");
-	const evalTooltipTrigger = document.querySelector("#vvp-evaluation-period-tooltip-trigger");
+	const periodEnd = new Date(parseInt(document.querySelector("#vvp-eval-end-stamp").innerText));
 
-	let periodStart, periodEnd;
-	if (evalStartStamp && evalEndStamp && evalTooltipTrigger) {
-		periodStart = new Date(parseInt(evalStartStamp.innerText));
-		periodEnd = new Date(parseInt(evalEndStamp.innerText));
-		evalTooltipTrigger.innerText = `Evaluation period: ${periodStart.toLocaleDateString()} - ${periodEnd.toLocaleString()}`;
+	if (document.querySelector("#vvp-evaluation-date-string > strong")) {
+		document.querySelector("#vvp-evaluation-date-string > strong").innerText = periodEnd.toLocaleString();
 	}
 
-	const percReviewedElement = document.querySelector("#vvp-perc-reviewed-metric-display strong");
-	const numReviewedElement = document.querySelector("#vvp-num-reviewed-metric-display strong");
-
-	if (!percReviewedElement || !numReviewedElement) {
-		return;
-	}
-
-	const percent = Math.round(parseFloat(percReviewedElement.innerText));
+	const percent = Math.round(
+		parseFloat(
+			document.querySelector(
+				"#vvp-perc-reviewed-metric-display p:nth-child(-n+2) *:is(strong, .a-size-extra-large)"
+			).innerText
+		)
+	); //Vine seems to always round to whole percent but sometimes displays inaccurate tenths
 	if (percent > 0) {
-		const count = parseInt(numReviewedElement.innerText.replace(/,/g, ""));
+		const metricsBox = document.querySelector("#vvp-vine-activity-metrics-box, #vvp-vine-account-details-box");
+		metricsBox.dataset.percent = percent;
+		const count = parseInt(
+			document
+				.querySelector("#vvp-num-reviewed-metric-display p:nth-child(-n+2) *:is(strong, .a-size-extra-large)")
+				.innerText.replaceAll(/\D+/g, "")
+		); //review count should alwasy be integer remove possible thousands separator
+		metricsBox.dataset.count = count;
 		const orderCount = Math.round((count / percent) * 100);
-		const orderMin = Math.min(Math.ceil((count / (percent + 0.5)) * 100), orderCount);
-		const orderMax = Math.max(Math.floor((count / (percent - 0.5)) * 100), orderCount);
-		const targetMin = Math.ceil(orderMin * 0.9) - count;
+		metricsBox.dataset.orderCountEstimate = orderCount;
+		// order min/max estimate assumes that percentage is rounded down
+		const orderMin = Math.floor((count / (percent + 1)) * 100) + 1;
+		metricsBox.dataset.orderCountMin = orderMin;
+		const orderMax = Math.max(Math.floor((count / percent) * 100), orderMin);
+		metricsBox.dataset.orderCountMax = orderMax;
+		const awaitingMin = orderMin - count;
+		metricsBox.dataset.awaitingCountMin = awaitingMin;
+		const awaitingMax = orderMax - count;
+		metricsBox.dataset.awaitingCountMax = awaitingMax;
+		const targetMin = Math.max(Math.ceil(orderMin * 0.9) - count, 0);
+		metricsBox.dataset.targetMin = targetMin;
 		const targetMax = Math.ceil(orderMax * 0.9) - count;
+		metricsBox.dataset.targetMax = targetMax;
 		const orderEstimate = orderMin == orderMax ? orderMax : `${orderMin}&ndash;${orderMax}`;
+		const awaitingEstimate = awaitingMin == awaitingMax ? awaitingMax : `${awaitingMin}&ndash;${awaitingMax}`;
 		const targetRequired = targetMin == targetMax ? targetMax : `${targetMin}&ndash;${targetMax}`;
-
-		const percDisplayP = document.querySelector("#vvp-perc-reviewed-metric-display p");
-		if (percDisplayP) {
-			if (targetMax > 0) {
-				percDisplayP.innerHTML = `You have reviewed <strong>${percent}%</strong> of ${orderEstimate} items; review ${targetRequired} more to reach 90%`;
-			} else {
-				percDisplayP.innerHTML = `You have reviewed <strong>${percent}%</strong> of ${orderEstimate} Vine items this period`;
-			}
+		if (awaitingMax > 0) {
+			document.querySelector(
+				"#vvp-num-reviewed-metric-display p:nth-child(-n+2):has(strong, .a-size-extra-large"
+			).innerHTML = `<span class="a-size-extra-large">${count} </span> ${awaitingEstimate} items await review`;
 		}
 
-		if (periodStart && periodEnd) {
-			const periodFraction = (new Date().setUTCHours(0, 0, 0, 0) - periodStart) / (periodEnd - periodStart);
-			if (periodFraction > 0) {
-				const awaitingEstimate = orderMax - count;
-				const projectedCount = count / periodFraction;
-				const projectedOrders = orderMin / periodFraction;
-				const projectedPercent = (projectedOrders - awaitingEstimate) / projectedOrders;
-				const countBar = document.querySelector("#vvp-num-reviewed-metric-display .animated-progress span");
-				const percentBar = document.querySelector("#vvp-perc-reviewed-metric-display .animated-progress span");
-				if (countBar) {
-					if (projectedCount < 70) {
-						countBar.style.backgroundColor = "red";
-					} else if (projectedCount < 77) {
-						countBar.style.backgroundColor = "orange";
-					} else if (projectedCount < 80) {
-						countBar.style.backgroundColor = "yellow";
-					}
-				}
+		if (targetMax > 0) {
+			document.querySelector(
+				"#vvp-perc-reviewed-metric-display p:nth-child(-n+2):has(strong, .a-size-extra-large"
+			).innerHTML =
+				`<span class="a-size-extra-large">${percent}%</span> of ${orderEstimate} items; ${targetRequired} more required to reach 90%`;
+		} else {
+			document.querySelector(
+				"#vvp-perc-reviewed-metric-display p:nth-child(-n+2):has(strong, .a-size-extra-large"
+			).innerHTML = `<span class="a-size-extra-large">${percent}%</span> of ${orderEstimate} items`;
+		}
 
-				if (percentBar) {
-					if (projectedPercent < 0.8) {
-						percentBar.style.backgroundColor = "red";
-					} else if (projectedPercent < 0.88) {
-						percentBar.style.backgroundColor = "orange";
-					} else if (projectedPercent < 0.92) {
-						percentBar.style.backgroundColor = "yellow";
-					}
-				}
+		const periodStart = new Date(parseInt(document.querySelector("#vvp-eval-start-stamp").innerText));
+		const periodFraction = (new Date().setUTCHours(0, 0, 0, 0) - periodStart) / (periodEnd - periodStart);
+		if (periodFraction > 0 && periodFraction < 1) {
+			const projectedCount = count / periodFraction;
+			metricsBox.dataset.projectedReviewCount = projectedCount.toFixed(1);
+			const projectedOrders = orderCount / periodFraction;
+			metricsBox.dataset.projectedOrderCount = projectedOrders.toFixed(1);
+			const projectedPercent = (projectedOrders - awaitingMax) / projectedOrders;
+			metricsBox.dataset.projectedReviewPercent = (projectedPercent * 100).toFixed(1);
+			const countBar = document.querySelector(
+				"#vvp-num-reviewed-metric-display [class*='animated-progress'] span"
+			);
+			const percentBar = document.querySelector(
+				"#vvp-perc-reviewed-metric-display [class*='animated-progress'] span"
+			);
+
+			if (projectedCount < 70) {
+				countBar.style.backgroundColor = "red";
+			} else if (projectedCount < 77) {
+				countBar.style.backgroundColor = "orange";
+			} else if (projectedCount < 80) {
+				countBar.style.backgroundColor = "yellow";
+			}
+
+			if (projectedPercent < 0.8) {
+				percentBar.style.backgroundColor = "red";
+			} else if (projectedPercent < 0.88) {
+				percentBar.style.backgroundColor = "orange";
+			} else if (projectedPercent < 0.92) {
+				percentBar.style.backgroundColor = "yellow";
 			}
 		}
 	}
