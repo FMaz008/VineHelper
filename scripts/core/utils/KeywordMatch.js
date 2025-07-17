c57539076e948f508efdf3b0c3b5248ca773197f
Francois Mazerolle <fmaz008@gmail.com>
2025-07-17 14:14:46 -0300

    Standardize that the unknown ETV value is null and not ""

--- scripts/core/utils/KeywordMatch.js
+++ scripts/core/utils/KeywordMatch.js
@@ -236,7 +236,7 @@ class KeywordMatcher {
 		}
 
 		// Check ETV filtering
-		if (word.etv_min === "" && word.etv_max === "") {
+		if (word.etv_min === null && word.etv_max === null) {
 			// No ETV filtering defined, we have a match
 			return true;
 		}
